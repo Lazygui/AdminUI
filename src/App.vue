46dfdd48d7162d38faa91fd887e9b90354454d48
zhouzhijun <15642532452@163.com>
2025-03-31 14:48:45 +0800

    feat: 初始化路由并添加类型支持
    - 在 App.vue 中添加 router-view 组件以支持路由
    - 在 main.ts 中引入并使用 router
    - 在 tsconfig.node.json 中添加路径别名配置
    - 在 vite.config.ts 中添加路径解析配置
    - 引入 @types/node 以提供类型支持

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-       <div class="min-h-screen min-w-screen"></div>
+       <div class="min-h-screen min-w-screen">
+              <router-view></router-view>
+       </div>
 </template>
 
 <style scoped lang="scss"></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +9,10 @@ export default defineConfig({
        server: {
               host: "0.0.0.0",
               port: 8080
+       },
+       resolve: {
+              alias: {
+                     "@": path.resolve(__dirname, "./src")
+              }
        }
 });
